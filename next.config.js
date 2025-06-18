e07ceaca2b29c57d4a5a3da25bf904fb7368f999
AKD <akinsipeoluwademilade@gmail.com>
2025-06-18 11:07:28 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images:{
-    domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
+    domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com","imageio.forbes.com","images.prismic.io"
+      
+    ]
   }
 }
 
--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -16,12 +16,12 @@ const getData = async () => {
 };
 
 const CategoryList = async () => {
-  const data = await getData();
+  const Data = await getData();
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
       <div className={styles.categories}>
-        {data?.map((item) => (
+        {Data.map((item) => (
           <Link
             href="/blog?cat=webdev"
             className={`${styles.category} ${styles[item.slug]}`}
